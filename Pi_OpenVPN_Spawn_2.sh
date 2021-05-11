01674d272a64288a5538b04dca406b0b59dad21f
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-11 20:03:34 +0300

    Create Pi_OpenVPN_Spawn_2.sh

---
+++ Pi_OpenVPN_Spawn_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+choice=n
+
+while [[ ${choice} != [y/Y] ]];
+do
+read -p "What spawn is this : " spawn
+echo "Is this spawn #${spawn}?"
+read -p "y/[n] : " choice
+done
+
+sudo ls >/dev/null 
+read -p "Press ENTER to continue"
+
+sudo mv /tmp/client* /etc/openvpn/
+
+sudo systemctl start openvpn@client${spawn}
