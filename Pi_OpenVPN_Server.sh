17d58471ef1bef93d98cdd1bcf40aa34ad540266
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-11 15:43:42 +0300

    Update Pi_OpenVPN_Server.sh

--- Pi_OpenVPN_Server.sh
+++ Pi_OpenVPN_Server.sh
@@ -50,7 +50,39 @@ EOF
 
 # Generate SSH-Keys for future script
   ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -N ""
-
+  
+# Copy SSH Public Keys to Spawns
+  choice=n
+  
+  while [[ ${choice} != [y/Y] ]];
+  do
+  read -p "Enter Spawn One IP : " spawn_one_ip
+  read -p "Enter Spawn Two IP : " spawn_two_ip
+  echo "${spawn_one_ip} and ${spawn_two_ip} : Are these correct?"
+  read -p "y/[n]" choice
+  done
+  
+  ssh-copy-id -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vpnuser@${spawn_one_ip}
+  read -p "Press ENTER to continue" ENTER
+  
+  ssh-copy-id -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vpnuser@${spawn_two_ip}
+  read -p "Press ENTER to continue" ENTER
+  
+# Creating SSH Config File
+  sudo tee -a ~/.ssh/config << EOF
+Host spawn1
+        Hostname ${spawn_one_ip}
+        Port 22
+        User vpnuser
+        IdentityFile ~/.ssh/id_rsa
+        
+Host spawn2
+        Hostname ${spawn_two_ip}
+        Port 22
+        User vpnuser
+        IdentityFile ~/.ssh/id_rsa
+EOF
+  
 # Install GIT & OpenVPN
   sudo apt-get install openvpn git -y
 
@@ -59,8 +91,17 @@ EOF
   sudo git clone https://github.com/OpenVPN/easy-rsa.git
   sudo chown vpnuser:vpnuser /opt/easy-rsa -R
   
-# Create Certificates
+# Create CA Certificate
   cd /opt/easy-rsa/easyrsa3
   ./easyrsa init-pki
   ./easyrsa --batch "--req-cn=openvpn.server" build-ca nopass
   
+# Create Server Certificate
+  ./easyrsa build-server-full vpn-server nopass
+  
+# Create Two Client Certificates
+  ./easyrsa build-client-full vpn-client-1 nopass
+  ./easyrsa build-client-full vpn-client-2 nopass
+
+# Create the DH Key
+  ./easyrsa gen-dh
