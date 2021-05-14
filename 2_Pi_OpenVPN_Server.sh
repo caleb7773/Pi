9e299756a0c10188f0cc696b6e566fb26277406c
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-15 01:45:51 +0300

    Update 2_Pi_OpenVPN_Server.sh

--- 2_Pi_OpenVPN_Server.sh
+++ 2_Pi_OpenVPN_Server.sh
@@ -2,14 +2,6 @@
 
 clear
 
-# Grabbing new users Sudo Password
-  sudo ls >/dev/null  
-  read -p " Press ENTER to continue" enter
-  clear
-  
-# Generate SSH-Keys for future script
-  ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -N ""
-  
 # Copy SSH Public Keys to Spawns
   choice=n
   
@@ -26,6 +18,66 @@ clear
  done
  
 
+clear
+
+# Grab the sudo password prior to advancing
+  echo "Put in Sudo Password"
+  sudo ls >/dev/null 
+  read -p " Press ENTER to continue" enter
+  clear
+
+# Create new vpn user
+  sudo useradd vpnuser
+
+# Change users password  
+  sudo sh -c 'echo vpnuser:vpnuserpassword | chpasswd'
+
+# Add user to Sudo Group
+  sudo usermod -aG sudo vpnuser
+
+# Give user access to bash shell by default
+  sudo sed -i 's/vpnuser:\/bin\/sh/vpnuser:\/bin\/bash/g' /etc/passwd
+
+# Make users Home Directory  
+  sudo mkdir /home/vpnuser
+
+# Give ownership to new user
+  sudo chown vpnuser /home/vpnuser
+  
+# Give group ownership to new user
+  sudo chgrp vpnuser /home/vpnuser
+
+# Update the system
+  sudo apt-get update
+
+
+# Delete default Kali User
+sudo vim -E -s /etc/passwd << EOF
+:g /kali/d
+:wq
+EOF
+
+# Switching to new user
+  clear
+  echo "Switch users to vpnuser"
+  su vpnuser     # Enter vpnuserpassword
+  read -p " Press ENTER to continue" enter
+  clear
+# Grabbing new users Sudo Password
+  sudo ls >/dev/null 
+  read -p " Press ENTER to continue" enter
+  clear
+  
+# Change ownership of git folder
+  sudo chown vpnuser /home/kali/pi -R
+  sudo chgrp vpnuser /home/kali/pi -R
+
+# Install OpenVPN
+  sudo apt-get install openvpn -y
+  
+# Generate SSH-Keys for future script
+  ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -N ""
+  
   clear
   ssh-copy-id -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vpnuser@${spawn_one_ip}
   read -p "Press ENTER to continue" ENTER
@@ -107,8 +159,7 @@ ${serverkey}
 </key>
 EOF
 
-sudo systemctl start openvpn@vpn-server
-sudo systemctl stop openvpn@vpn-server
+
 sudo systemctl enable openvpn@vpn-server
 sudo systemctl start openvpn@vpn-server
   
@@ -174,3 +225,5 @@ EOF
 
 
 echo " Complete... "
+
+ping 10.99.99.1
