2dcc9eb820be28494f630e799f6b35490cb4fb3c
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-15 02:12:32 +0300

    Update 1_Pi_OpenVPN_Spawn.sh

--- 1_Pi_OpenVPN_Spawn.sh
+++ 1_Pi_OpenVPN_Spawn.sh
@@ -31,7 +31,7 @@ clear
   sudo chgrp vpnuser /home/vpnuser
 
 # Update the system
-  sudo apt-get update
+#  sudo apt-get update
 
 
 # Delete default Kali User
@@ -56,7 +56,7 @@ EOF
   sudo chgrp vpnuser /home/kali/pi -R
 
 # Install OpenVPN
-  sudo apt-get install openvpn -y
+#  sudo apt-get install openvpn -y
   
 
 looper() {
