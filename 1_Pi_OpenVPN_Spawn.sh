1e02391deeb99c702c33f9013fba59479013cb51
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-15 02:24:19 +0300

    Update 1_Pi_OpenVPN_Spawn.sh

--- 1_Pi_OpenVPN_Spawn.sh
+++ 1_Pi_OpenVPN_Spawn.sh
@@ -33,13 +33,6 @@ clear
 # Update the system
 #  sudo apt-get update
 
-
-# Delete default Kali User
-sudo vim -E -s /etc/passwd << EOF
-:g /kali/d
-:wq
-EOF
-
 # Switching to new user
   clear
   echo "Switch users to vpnuser"
@@ -77,5 +70,11 @@ looper
 sleep 1s
 echo "Still waiting..."
 done
-    
-  ping 10.99.99.1
+
+# Delete default Kali User
+sudo vim -E -s /etc/passwd << EOF
+:g /kali/d
+:wq
+EOF
+
+ping 10.99.99.1
